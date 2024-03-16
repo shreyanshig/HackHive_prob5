0168f96f382d04e880c64e617259e6a22c973d36
Shreyanshi <cherrygolhani@gmail.com>
2024-03-16 18:22:26 +0530

    create login controllers

---
+++ src/java/Controller/RegisterInvigControllerServlet.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import dao.AdminDAO;
+import dao.EVAppDAO;
+import dto.IVDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Shreyanshi
+ */
+public class RegisterInvigControllerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher rd = null;
+        HttpSession session = request.getSession();
+        String userid = (String)session.getAttribute("userid");
+        if(userid == null){
+            session.invalidate();
+            response.sendRedirect("accessdenied.html");
+            return;
+        }
+        String username = (String)request.getParameter("userid");
+        String password = (String)request.getParameter("password");
+        IVDTO invig = new IVDTO();
+        invig.setUsername(username);
+        invig.setPassword(password);
+        System.out.println("Inside Registerinvigcontrollerservlet");
+        System.out.println(invig.getUsername()+" "+invig.getPassword());
+        try
+        {
+            boolean tf = AdminDAO.addinvig(invig);
+            request.setAttribute("result", tf);
+            rd = request.getRequestDispatcher("showinvigresult.jsp");
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            System.out.println("Some exception ocuured in EVAnswerSheetControllerServlet"+ex);
+            request.setAttribute("Exception", ex);
+            rd = request.getRequestDispatcher("showexception.jsp");
+        }
+        finally{
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
